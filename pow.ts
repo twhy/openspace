ffda6eef450225088875a0eb3209ef6228a0b98e
Tom Wang <tom.wanghaiyi@gmail.com>
2024-07-02 20:25:39 +0800

    Date.now()

--- pow.ts
+++ pow.ts
@@ -4,14 +4,13 @@ const username = 'twhy';
 
 function pow(nzero: number = 0) {
   const zeros = new Array(nzero).fill(0).join('');
-  const start = +new Date();
+  const start = Date.now();
   while (true) {
     const nonce = randomBytes(30).toString('hex');
     const data = username + nonce;
     const hash = createHash('sha256').update(data).digest('hex');
     if (hash.startsWith(zeros)) {
-      const end = +new Date();
-      const time = end - start;
+      const time = Date.now() - start;
       return { time, data, hash };
     }
   }
